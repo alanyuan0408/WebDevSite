45831fcd46fa2c944718356974fd7df78afe3e0c
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-07-31 15:35:09 -0400

    users can sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
 
-	
+	def show
+    	@currentPage = {:useraccount => "active"};
+  		@user = User.find(params[:id])
+  	end
+
 	def new
 		@currentPage = {:useraccount => "active"};
 	end
 
 	def create
 		@currentPage = {:useraccount => "active"};
-		user = User.find_by_email(params[:name][:email])
-		if user && user.authenticate(params[:name][:email])
+		@user = User.find_by_name(params[:session][:name])
+		if @user.email == params[:session][:email]
 		# Sign the user in and redirect to the user's show page.
 			redirect_to @user
 		else
-			raise "Username and email do not match"
+			@currentPage = {:usererror => "true"};
+			@user.errors.add(:email, "email does not match")
 			render 'new'
 		end
 	end
