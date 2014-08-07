1ce89cb1d21a0bdec731e649c6d68b3ae9c23740
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-08-07 12:18:10 -0400

    password_digest

--- db/migrate/20140805144438_add_password_digest_to_users.rb
+++ db/migrate/20140805144438_add_password_digest_to_users.rb
@@ -1,6 +1,6 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :password, :string
+    add_column :users, :password_digest, :string
     add_column :users, :password_confirmation, :string
   end
 end
