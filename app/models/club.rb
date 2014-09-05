860625dca63fdb191268047611b9bcee1394e216
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-05 15:16:06 -0400

    seed

--- app/models/club.rb
+++ app/models/club.rb
@@ -3,4 +3,9 @@ class Club < ActiveRecord::Base
 
   attr_accessible :date, :content, :title, :send
 
+  validates :sendd, presence: true, length: {maximum: 40}
+  validates :content, presence: true, length: {maximum: 400}
+  validates :title, presence: true, length: {maximum: 40}
+  validates :date, presence: true, length: {maximum: 40}
+
 end
