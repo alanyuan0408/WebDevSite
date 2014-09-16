bb1cab3690bc6205b98a1f010261908eae145b92
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-15 20:15:01 -0400

    changed update settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
 
   validates :name, 	presence: true, uniqueness: true
   validates :email,	presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password_confirmation, presence: true, on: :create
+  validates :email_frequency, presence: true, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 7 }
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token #create an admin user
