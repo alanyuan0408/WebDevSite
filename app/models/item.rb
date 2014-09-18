ea131bbabbd3035394a1e4618f495ae9e4b6f874
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-18 18:59:20 -0400

    removed limit

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   attr_accessible :contact, :content, :title, :type_of
 
   validates :contact, presence: true, length: {maximum: 40}
-  validates :content, presence: true, length: {maximum: 1000}
+  validates :content, presence: true
   validates :title, presence: true, length: {maximum: 40}
   validates :type_of, presence: true, length: {maximum: 40}
 
