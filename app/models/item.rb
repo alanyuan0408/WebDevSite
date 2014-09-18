7b828b0dceb34d76705ea447f6a6e41048c187e3
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-18 18:49:55 -0400

    increse item limit

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   attr_accessible :contact, :content, :title, :type_of
 
   validates :contact, presence: true, length: {maximum: 40}
-  validates :content, presence: true, length: {maximum: 400}
+  validates :content, presence: true, length: {maximum: 1000}
   validates :title, presence: true, length: {maximum: 40}
   validates :type_of, presence: true, length: {maximum: 40}
 
