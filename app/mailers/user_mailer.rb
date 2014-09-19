e0022bc75dd65700106558f5474b91a22d0e6480
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-19 18:47:26 -0400

    bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,6 +13,8 @@ class UserMailer < ActionMailer::Base
       @YNCNPosts = Item.where(created_at: (Time.now - @user.email_frequency.days)..Time.now).where(type_of: "YNCNPost")
       @clubPosts = Item.where(created_at: (Time.now - @user.email_frequency.days)..Time.now).where(type_of: "ClubPost")
       mail(to: @user.email, subject: 'Automated Web Club Email')
+    else 
+      @user = user
     end
   end
 
