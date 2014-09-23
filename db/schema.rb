c43c92a49c9ef45928e7c8cd6ac8a91002850d1e
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-09-22 21:53:49 -0400

    modified CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140920164041) do
+ActiveRecord::Schema.define(:version => 20140922220950) do
 
   create_table "items", :force => true do |t|
     t.string   "title"
     t.string   "contact"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "type"
     t.string   "type_of"
     t.text     "content"
+    t.text     "information"
   end
 
   create_table "users", :force => true do |t|
