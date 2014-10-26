920db869f0410c071f228602ba88d4d9bc0eff72
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-10-25 23:46:24 -0400

    downloaded summernote

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module UoftWebSite
     # config.autoload_paths += %W(#{config.root}/extras)
     config.assets.initialize_on_precompile = false
 
+    config.assets.css_compressor = :yui
     config.assets.js_compressor = :uglify
 
     # Only load the plugins named here, in the order given (default is alphabetical).
