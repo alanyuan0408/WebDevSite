382c9cdbb7f12f434aadf730fe566830a315497b
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-10-26 00:08:09 -0400

    serve_static_assets

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ module UoftWebSite
     # config.autoload_paths += %W(#{config.root}/extras)
     config.assets.initialize_on_precompile = false
 
-    config.assets.css_compressor = :yui
-    config.assets.js_compressor = :uglify
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
