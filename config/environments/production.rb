c1e273de9c758a4f58893f92fdba5fdacd7aba4f
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-10-26 00:58:01 -0400

    index.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,11 @@ UoftWebSite::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
