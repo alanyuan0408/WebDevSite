075004ae1c3d900f2300783b4736bc8a2b18dbbd
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-10-30 19:53:00 -0400

    modified migrate

--- db/migrate/20141028195357_new_column.rb
+++ db/migrate/20141028195357_new_column.rb
@@ -2,7 +2,7 @@ class NewColumn < ActiveRecord::Migration
 
   def change
   	add_column :users, :expo_ticket, :boolean, :default => false
-  	add_column :users, :nextsend, :datetime, :default => true
+  	add_column :users, :nextsend, :datetime
   	add_column :users, :account_created, :boolean, :default => false
   end
 
