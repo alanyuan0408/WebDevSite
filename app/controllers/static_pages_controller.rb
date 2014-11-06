712cf461c7d99fd3088f80f2a364f8f2a3009f1d
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-11-06 14:17:07 -0500

    added cookie users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,61 +1,61 @@
 class StaticPagesController < ApplicationController
  
-  def home
-    @currentPage = {:home => "active"};
+  helper_method :login_method
+
+  def login_method
+    current_user
+
+    if signed_in?
+      sign_in(@current_user)
+    end
+
     if session[:remember_token] && User.exists?(:id => session[:remember_token])
       user = User.find(session[:remember_token])
       @user_name = user.name
     else
       @user_name = "Account Login"
     end
+  end
+
+  def home
+    login_method
+
+    @currentPage = {:home => "active"};
   end
 
   def developer
+    login_method
+
     @currentPage = {:developer => "active"};
-    if session[:remember_token] && User.exists?(:id => session[:remember_token])
-      user = User.find(session[:remember_token])
-      @user_name = user.name
-    else
-      @user_name = "Account Login"
-    end
   end
 
   def entrepreneur
+    login_method
+
     @currentPage = {:entrepreneur => "active"};
     @number_of_participants = User.where(:expo_ticket => true).length;
-
-    if session[:remember_token] && User.exists?(:id => session[:remember_token])
-      @user = User.find(session[:remember_token])
-      @user_name = @user.name
-    else
-      @user_name = "Account Login"
-    end
   end
 
   def events
+    login_method
+
     @currentPage = {:events => "active"};
-    if session[:remember_token] && User.exists?(:id => session[:remember_token])
-      user = User.find(session[:remember_token])
-      @user_name = user.name
-    else
-      @user_name = "Account Login"
-    end
   end
 
   def jobs
+    login_method
+
     @currentPage = {:jobs => "active"};
-    if session[:remember_token] && User.exists?(:id => session[:remember_token])
-      user = User.find(session[:remember_token])
-      @user_name = user.name
-    else
-      @user_name = "Account Login"
-    end
-
     @jobPosts = Item.where(type_of: "JobPost").order("id desc")
-
   end
 
   def useraccount
+    current_user
+
+    if signed_in?
+      sign_in(@current_user)
+    end
+
     @currentPage = {:useraccount => "active"};
     if session[:remember_token] && User.exists?(:id => session[:remember_token])
       @user = User.find(session[:remember_token])
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ UoftWebSite::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
