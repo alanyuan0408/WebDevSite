bd5be8a9db5d745264ad29e0e27899660878f6ca
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-12-05 13:14:27 -0500

    fixed jobs

---
+++ db/seed.rb
@@ -0,0 +1,9 @@
+#t.string   "item_id"
+#t.string   "item_url"
+#t.string   "item_title"
+#t.datetime "item_date"
+#t.text     "item_text"
+#t.integer  "column_type"
+
+Feedbank.create("item_id" => "1", "item_url"=> "http://www.xyzpub.com",
+	"item_date" => )
