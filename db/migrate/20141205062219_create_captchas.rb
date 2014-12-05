21d463bc3bc2cb387f4067ae2765ae9f372aa00f
Alan Yuan <alanyuan_ca@yahoo.ca>
2014-12-05 13:23:20 -0500

    fixed typo

--- db/migrate/20141205062219_create_captchas.rb
+++
@@ -1,8 +0,0 @@
-class CreateCaptchas < ActiveRecord::Migration
-  def change
-    create_table :captchas do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/seed.rb
+++ db/seed.rb
@@ -5,5 +5,19 @@
 #t.text     "item_text"
 #t.integer  "column_type"
 
-Feedbank.create("item_id" => "1", "item_url"=> "http://www.xyzpub.com",
-	"item_date" => )
+job_list = [
+  [ "title 1", 81831000 ],
+  [ "title 2", 65447374 ],
+  [ "title 3", 10839905 ],
+  [ "title 4", 16680000 ],
+  [ "title 5", 11831000 ],
+  [ "title 6", 85447374 ],
+  [ "title 7", 11839905 ],
+  [ "title 8", 16620000 ]
+]
+job_list.each do |name, population|
+  Feedbank.create(item_id: population, "item_url"=> "http://www.xyzpub.com",
+	"item_date" => DateTime.now, item_text: name, "column_type" => "1")
+end
+
+
